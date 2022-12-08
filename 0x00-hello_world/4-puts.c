f6ca044bbe4bb007ad963f048c00ad468fd09851
Thomas Y. Melka <tymelka@gmail.com>
2022-12-08 11:21:28 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: 0
+ */
 int main (void)
 {
     puts("Programming is like building a multilingual puzzle\n");
