1980756fd3843f82f0c15e78da82a82c5ed948bf
Thomas Y. Melka <tymelka@gmail.com>
2022-12-11 04:19:01 -0800

    task2 betty style doc

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,8 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
@@ -15,5 +19,5 @@ for(x = 'a'; x <= 'z'; x++)
 putchar(x);
 }
 putchar ('\n');
- return 0;
+return 0;
 }
