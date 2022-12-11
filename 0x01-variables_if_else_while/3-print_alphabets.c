7ad59504007df2ccb53e23fafe3f002e1d47a4bc
Thomas Y. Melka <tymelka@gmail.com>
2022-12-11 04:53:57 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(x);
+}
+ putchar ('\n');
+return (0);
+}
