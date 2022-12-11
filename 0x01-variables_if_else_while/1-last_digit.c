d229419c8005d3e7255fd3a37b95e8f0c4a230da
Thomas Y. Melka <tymelka@gmail.com>
2022-12-11 00:27:45 -0800

    task 1 correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,10 +23,11 @@ printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("Last digit of %d is %d and is 0\n",n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
-else if ((last_digit<6) && (last_digit != 0))
+else if (last_digit <6 && last_digit != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-}return (0);
+}
+ return (0);
 }
