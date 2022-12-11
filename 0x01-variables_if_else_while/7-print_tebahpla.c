46920f08bcc9e7e78f9f8e213218fe64101d626e
Thomas Y. Melka <tymelka@gmail.com>
2022-12-11 12:39:51 -0800

    var...task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int x;
+for (x = 122 ; x >= 97 ; --x)
+{
+putchar(x);
+}
+putchar ('\n');
+return (0);
+}
