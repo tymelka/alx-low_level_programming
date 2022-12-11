dc388cf2f3aca2b37b0f15c07b27729af1262262
Thomas Y. Melka <tymelka@gmail.com>
2022-12-11 03:20:57 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+char ralpha;
+for(int x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+return 0;
+}
