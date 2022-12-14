ce28974c41503fd67b08ebc18d67f14048f5b612
Thomas Y. Melka <tymelka@gmail.com>
2022-12-13 19:18:00 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,9 @@
 
 /**
  *  print_alphabet_x10 - Entry point
- * 
+ *
  * Description: 'print alphabet in small letter ten times'
- * 
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
