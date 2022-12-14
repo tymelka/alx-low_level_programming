cc71e64cf24f1bd753af1b26abb47b39c2b6d494
Thomas Y. Melka <tymelka@gmail.com>
2022-12-14 12:17:16 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * add - add two integer numbers
+ * @n: integer number
+ * Return: the last digit of number
+ */
+int add(int n1, int n2)
+  
+{
+  return (n1+n2);
+}
