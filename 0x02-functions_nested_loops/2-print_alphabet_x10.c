ac8c532e2554fd289bf5f12b8a573def1a5c9a87
Thomas Y. Melka <tymelka@gmail.com>
2022-12-13 19:14:26 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
-
 /**
- * main - Entry point
+ *  print_alphabet_x10 - Entry point
  * 
- * Description: 'the program's description'
+ * Description: 'print alphabet in small letter ten times'
  * 
  * Return: Always 0 (Success)
  */
-
 void print_alphabet_x10(void)
 {
 int i, n;
