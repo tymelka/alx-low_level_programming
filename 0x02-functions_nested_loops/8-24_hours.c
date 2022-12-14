f67587d81bb3c1264c180f04a4a23306e0e7c786
Thomas Y. Melka <tymelka@gmail.com>
2022-12-14 11:53:26 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * jack_bauer - a function that returns hours and minutes
+ * 
+ * Return: retunrs 0
+ */
+void jack_bauer(void)
+
+int h, m;
+{
+for (h == 0; h <= 23; h++)
+{
+for (m == 0; m <= 59; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
+}
