533577fe6a2d1ba44fe6c512db3ade312edb0aa2
Thomas Y. Melka <tymelka@gmail.com>
2022-12-16 10:52:41 -0800

    task 0

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * _isdigit - Check if is digit
  * @a: First Integer
  * @b: Second Integer
  *
- * Return: REturns the multiplied value
+ * Return: Rturns the multiplied value
  */
 int mul(int a, int b)
 
