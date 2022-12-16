daf438659d9e9aaffdd04d78d95bab80ae69e73b
Thomas Y. Melka <tymelka@gmail.com>
2022-12-16 10:45:26 -0800

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - Check if is digit
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: REturns the multiplied value
+ */
+int mul(int a, int b);
+{
+
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _isupper(int);
 int _isdigit(int c);
+int mul(int, int);
 
 #endif
