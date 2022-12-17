aba6b1701ed5e98520430bd1b06e1b30b23b7f58
Thomas Y. Melka <tymelka@gmail.com>
2022-12-17 07:42:22 -0800

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,16 @@
 #ifndef BLAH
 #define BLAH
 
-int _putchar(char);
+int _putchar(char)
 int _isupper(int);
-int _isdigit(int c);
+int _isdigit(int);
 int mul(int, int);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);void print_triangle(int size);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
 
 
 #endif
