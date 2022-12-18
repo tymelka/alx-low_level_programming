5bd5c6f6f735b70d0b3b97b2600fc008ffa2645b
Thomas Y. Melka <tymelka@gmail.com>
2022-12-18 10:09:33 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,39 +4,38 @@
 #include "main.h"
 
 /**
- * jack_bauer - a function that returns hours and minutes
+ * times_table - prints 9 times table
  *
  * Return: retunrs 0
  */
 void times_table(void)
 {
-  int rone, cone, d;
+int rone, cone, d;
 
-  for (rone = 0; rone <= 9; rone++)
-    {
-      _putchar('0');
-      _putchar(',');
-      _putchar(' ');
-      for (cone = 1; cone <= 9; cone++)
-	{
-	  d = (rone * cone);
-	  if ((d / 10) > 0)
-	    {
-	      _putchar((d / 10) + '0');
-	    }
-	  else
-	    {
-	      _putchar(' ');
-	    }
-	  _putchar((d % 10) + '0');
-
-	  if (cone < 9)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-	}
-      _putchar('\n');
-    }
+for (rone = 0; rone <= 9; rone++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+for (cone = 1; cone <= 9; cone++)
+{
+d = (rone * cone);
+if ((d / 10) > 0)
+{
+_putchar((d / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((d % 10) + '0');
+if (cone < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 
 }
