5f40f5c406d34e3714d9e4748df38183245558bd
Thomas Y. Melka <tymelka@gmail.com>
2022-12-18 04:41:40 -0800

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,13 +17,13 @@ _putchar('\n');
 }
 else
 {
-for (i = 1; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 for (j = 0; j < i; j++)
 {
 _putchar(32);
 }
-_putchar(95);
+_putchar(92);
 _putchar('\n');
 }
 }
