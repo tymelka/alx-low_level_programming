c7559f14b8aa53ea087b634c829261fff7675736
Thomas Y. Melka <tymelka@gmail.com>
2022-12-19 13:24:33 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
 
 void print_rev(char *s)
 {
-  int i = 0;
+int i = 0;
 
-  while (*s != 0)
-    {
-    i++;
-    }
-  while (i >= 0)
-    {
-      _putchar(s[i]);
-      i--;
-    }
-  _putchar('\n');
+while (s[i] != 0)
+{
+i++;
+}
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
 }
