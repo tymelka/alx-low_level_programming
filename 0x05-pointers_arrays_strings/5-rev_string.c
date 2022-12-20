0b992098b2f3a687bd1377b9281cc47272c124c8
Thomas Y. Melka <tymelka@gmail.com>
2022-12-20 14:13:27 -0800

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,24 +6,28 @@
  * Return: Always 0 (Success)
  */
 
-void reverse_string(char *s)
+void rev_string(char *s)
 {
+  int i;
+  int j;
+  int temp;
+  int length;
 
-  int i,len,tmp;
-
-  len = 0;
-
-  while (*s != '\0')
+  while (s[i] != 0)
     {
-      len = len + 1;
-      s = s + 1;
+      i++;
     }
-  
 
-  for(i = 0;i < len/2; i++)
+  length = i;
+  i = 0;
+  j = length - 1;
+
+  while (i < j)
     {
-      tmp=s[i];
-      s[i]=s[len];
-      s[len--]=tmp;
+      temp = s[i];
+      s[i] = s[j];
+      s[j] = temp;
+      i++;
+      j--;
     }
 }
