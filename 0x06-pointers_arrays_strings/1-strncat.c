ba6600ab28284fb88451147438193d528ce73d04
Thomas Y. Melka <tymelka@gmail.com>
2022-12-22 03:03:45 -0800

    conact string n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+/**
+ * *_strncat - concatenates two strings
+ * @dest: string to be concatenated to
+ * @src: string to concatenate from
+ *
+ * Return: concatenated strings
+ */
+
+char _strncat(char *dest, char *src, int n)
+
+{
+int i, j;
+i = 0;
+j = 0;
+
+while (dest[i] != 0)
+{
+i++;
+}
+while (src[j] != 0 && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+return (*dest);
+}
