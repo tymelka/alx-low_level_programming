8e245a2fc51ec12e7a037a88cad39170fed605fc
Thomas Y. Melka <tymelka@gmail.com>
2022-12-22 12:14:09 -0800

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-  int a = 0;
-  int b = 0;
-
-  while (s1[a] != '\0' && b == 0)
-    {
-      b = s1[a] - s2[a];
-      a++;
-    }
-  return (b);
+int a = 0;
+int b = 0;
+while (s1[a] != '\0' && b == 0)
+{
+b = s1[a] - s2[a];
+a++;
+}
+return (b);
 }
