144fdb3555da51446632be67d1990398f6de7ba7
Thomas Y. Melka <tymelka@gmail.com>
2022-12-22 11:42:44 -0800

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 /**
- * *_strncpy - copies a string 
- * @dest: destination string to be copied to 
+ * *_strncpy - copies a string
+ * @dest: destination string to be copied to
  * @src: source of string to concatenate from
  * @n: number of times
  *
@@ -10,20 +10,17 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-  int i;
-
-  i = 0;
-  while (i < n && src[i] != '\0')
-    {
-      dest[i] = src[i];
-      i++;
-    }
-
-
-  while (i < n)
-    {
-      dest[i] = '\0';
-      i++;
-    }
-  return (dest);
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
