143cef6f36bd518eaa62a5d209d4cae9286f9cc3
Thomas Y. Melka <tymelka@gmail.com>
2022-12-23 03:46:26 -0800

    reverse order

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -10,17 +10,17 @@
  */
 void print_array(int *a, int n)
 {
-  int i;
+int i;
 
-  i = 0;
-  while (i < n)
-    {
-      if (i != 0)
-	{
-	  printf(", ");
-	}
-      printf("%d", a[i]);
-      i++;
-    }
-  printf("\n");
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
 }
