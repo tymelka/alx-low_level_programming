b3d066d53c42506a71a368ce0d1306474d67a690
Thomas Y. Melka <tymelka@gmail.com>
2023-01-04 03:52:01 -0800

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+  for (i = 0; i < n; i++)
+    {
+      s[i] = b;
+    }
+  return (s);
+  
+}
