456140d0c26ec8ab0a07b91678c2a8a03566f586
Thomas Y. Melka <tymelka@gmail.com>
2023-01-04 04:17:58 -0800

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _memcpy - prints buffer in hexa
+ * @src: the address of memory to print
+ * @n: the size of the memory to write into
+ * @dest: tthe constant to be written into s address
+ *
+ * Return: the memory address
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+unsigned int i;
+ 
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+
+}
