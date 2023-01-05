e675df58a9752a8dab34bf428eba75fb1bf2166c
Thomas Y. Melka <tymelka@gmail.com>
2023-01-05 14:57:30 -0800

    5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - compute square root
+ * @n: input
+ * Return: the square root
+ */
+
+int _sqrt(int rem, int n)
+{
+if (rem <= n / 2)
+{
+if (n == 1)
+return n;
+if (rem * rem != n)
+return (_sqrt(rem + 1, n));
+return (rem);
+}
+return (-1);
+}
+
+int _sqrt_recursion(int n)
+{
+  return (_sqrt(0, n));
+}
