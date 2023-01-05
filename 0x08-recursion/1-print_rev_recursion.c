d0a5c7c8bc37b66397d22f03b356476e5897f7f9
Thomas Y. Melka <tymelka@gmail.com>
2023-01-04 16:03:08 -0800

    1-print_rev_recursion-betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 # include "main.h"
 /**
- * _print_rev_recursion - Entry point
+ * _print_rev_recursion - reverse write a string
  * @s: input
  * Return: Always 0 (Success)
  */
-  void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-  if (*s != '\0')
-    _print_rev_recursion(s + 1);
-  else
-    return;
-  _putchar(*s);
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+else
+return;
+_putchar(*s);
 }
